f0a8752c91f185a6b4f8577f51cdf44374da2da6
root <root@localhost.localdomain>
2022-11-19 16:48:44 -0800

    playing with timeout

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import docker
-client = docker.APIClient(base_url='unix://var/run/docker.sock')
+##client = docker.APIClient(base_url='unix://var/run/docker.sock'
+client = docker.APIClient(tcp://127.0.0.1:1234, timeout=10)
 pullresult = client.pull("alpine")
 listresult = client.containers(all='true')
 print(listresult)
